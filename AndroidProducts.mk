fabacc093fbfc4a37cfaed9f43bd289d2bd31e79
Lanmiemie <fatdeadpanda@163.com>
2023-01-30 09:44:25 +0800

    mi_lx04 device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_mi_lx04.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_mi_lx04-user \
+    lineage_mi_lx04-userdebug \
+    lineage_mi_lx04-eng
---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for XiaoMi LX04 (mi_lx04)
+
+```
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
